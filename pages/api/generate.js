87475c6ddc781d99b297a39c1006db1dbf659d77
Jason Cardinal <93737375+Metaluv@users.noreply.github.com>
2023-01-04 05:44:31 -0700

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -31,9 +31,9 @@ export default async function (req, res) {
       model: "text-davinci-003",
       prompt: generatePrompt(animal),
       temperature: 0.6,
-      max_tokens: 350,
+      max_tokens: 250,
     });
-    
+
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
