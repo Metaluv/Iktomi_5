bc2da19e53f005a1d4289cd8f69da9bbcb3d9fe7
Jason Cardinal <93737375+Metaluv@users.noreply.github.com>
2023-01-04 11:57:16 -0700

    update cupcake

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,9 +1,10 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  organization: "org-KcoN1dzPfBkt8kiQVAIy3Mo1",
+  organization: process.env.ORGANIZATION_ID,
   apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
@@ -20,7 +21,7 @@ export default async function (req, res) {
   if (animal.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter a valid topic",
       }
     });
     return;
@@ -29,9 +30,12 @@ export default async function (req, res) {
   try {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
-      max_tokens: 250,
+      prompt: `As professional native american story teller you explain in detail the ${animal} and create a short story that is friendly and appealing to all audiences using animals as characters and only using 750 words or less. You are free to use any animals you want, but you must use the ${animal} at least once. You must add at least two cree words with definitions of the word for learning. Check sentence structure, and grammar before submitting.`,
+      temperature: 0.5,
+      max_tokens: 1000,
+      top_p: 1,
+      frequency_penalty: 0.92,
+      presence_penalty: 0.87,
     });
 
     res.status(200).json({ result: response.data.choices[0].text });
@@ -50,9 +54,3 @@ export default async function (req, res) {
     }
   }
 }
-
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-    return `As professional native american story teller you explain in detail the ${capitalizedAnimal} and create a short story that is friendly and appealing to all audiences using animals as characters and only using 750 words or less. You are free to use any animals you want, but you must use the ${capitalizedAnimal} at least once. You must also use an earthly element at least once and it must be sciencifically correct. You must add at least two cree words with definitions of the word for learning. Check sentence structure, and grammar before submitting. This is for teach purposes. Add paragraph breaks with spacing where you feel they are needed.`;
-  }
