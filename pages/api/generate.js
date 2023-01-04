9e7743fbf4b35cebbfd8bdbc7c1149ff24b59def
Jason Cardinal <93737375+Metaluv@users.noreply.github.com>
2023-01-04 05:46:35 -0700

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -27,14 +27,14 @@ export default async function (req, res) {
   }
 
   try {
-    const completion = await openai.createCompletion({
+    const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: generatePrompt(animal),
       temperature: 0.6,
       max_tokens: 250,
     });
 
-    res.status(200).json({ result: completion.data.choices[0].text });
+    res.status(200).json({ result: response.data.choices[0].text });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
